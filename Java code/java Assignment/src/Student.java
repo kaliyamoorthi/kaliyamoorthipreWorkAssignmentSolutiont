9e9d102e214583fa23427f43ceb3defdcfb75b6d
kaliyamoorthi <99474003+kaliyamoorthi@users.noreply.github.com>
2022-02-11 20:26:57 +0530

    Add files via upload

---
+++ Java code/java Assignment/src/Student.java
@@ -0,0 +1,148 @@
+
+import java.util.Scanner;
+
+ 
+
+public class Main.main {
+
+ 
+
+Scanner sc = new Scanner(System.in);
+
+ 
+
+           //function to checkPalindrome
+
+       public void checkPalindrome()  {
+
+      }
+
+ 
+
+            //function to printPattern
+
+       public void printPattern() {
+
+      }
+
+ 
+
+          //function to check no is prime or not
+
+        public void checkPrimeNumber() {
+
+        }
+
+         
+
+          // function to print Fibonacci Series
+
+          public void printFibonacciSeries() {
+
+                //initialize the first and second value as 0,1 respectively.
+
+           int first = 0, second = 1;
+
+          }
+
+ 
+
+//main method which contains switch and do while loop
+
+      public static void main(String[] args) {
+
+                Main obj = new Main ();
+
+                int choice;
+
+                Scanner sc = new Scanner(System.in);
+
+do {
+
+System.out.println("Enter your choice from below list.\n" + "1. Find palindrome of number.\n"
+
++ "2. Print Pattern for a given no.\n" + "3. Check whether the no is a  prime number.\n"
+
++ "4. Print Fibonacci series.\n" + "--> Enter 0 to Exit.\n");
+
+System.out.println();
+
+ 
+
+choice = sc.nextInt();
+
+switch (choice) {
+
+ 
+
+case 0:
+
+choice = 0;
+
+break;
+
+ 
+
+case 1: {
+
+obj.checkPalindrome();
+
+}
+
+break;
+
+ 
+
+case 2: {
+
+ 
+
+obj.printPattern();
+
+}
+
+break;
+
+ 
+
+case 3: {
+
+obj.checkPrimeNumber();
+
+}
+
+break;
+
+ 
+
+case 4: {
+
+ 
+
+obj.printFibonacciSeries();
+
+}
+
+break;
+
+ 
+
+default:
+
+System.out.println("Invalid choice. Enter a valid no.\n");
+
+}
+
+ 
+
+} while (choice != 0);
+
+System.out.println("Exited Successfully!!!");
+
+sc.close();
+
+}
+
+ 
+
+}
